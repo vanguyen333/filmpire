d85c0a035aa297ef488aab55ba842615325ac06d
vanguyen333 <vanguyen333@yahoo.com>
2024-01-10 13:18:57 -0500

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import {Route,Switch} from 'react-router-dom';
+// import {Route,Switch} from 'react-router-dom';
 
 
 const App = () => {
