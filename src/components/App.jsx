460ccc12354af617e5504534828a40df44c039dd
vanguyen333 <vanguyen333@yahoo.com>
2024-01-11 11:40:25 -0500

    reinstalled react-router-dom, set up basic routers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import { CssBaseline } from '@mui/material';
-// import {Route,Switch} from 'react-router-dom';
-
+import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-    <CssBaseline/>
-    <h1>Hello, World - Filmpire</h1>
+      <CssBaseline />
+      <main>
+        <Switch>
+          <Route exact path="/movie/:id">
+            <h1>Movie Information</h1>
+          </Route>
+          <Route exact path="/actors/:id">
+            <h1>Actors</h1>
+          </Route>
+          <Route exact path="/">
+            <h1>Movies</h1>
+          </Route>
+          <Route exact path="/profile/:id">
+            <h1>Profile</h1>
+          </Route>
+        </Switch>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
