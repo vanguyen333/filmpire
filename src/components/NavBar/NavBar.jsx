beb43ba2c27c75fcb4cee9449d466dac2ac4d694
vanguyen333 <vanguyen333@yahoo.com>
2024-01-11 12:46:15 -0500

    nice blue navbar with no items

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Drawer,
+  Button,
+  Avatar,
+} from "@mui/material";
+import {
+  Menu,
+  AccountCircle,
+  Brightness4,
+  Brightness7,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+import useStyles from "./styles";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}></Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default NavBar;
