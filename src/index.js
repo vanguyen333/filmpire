f3b3ad0ce15393ab59d6b72f26b6692c777d4ff3
vanguyen333 <vanguyen333@yahoo.com>
2024-01-12 12:31:28 -0500

    added menuButton styles to navbar using Theme

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import App from './components/App';
+import App from "./components/App";
 
+const theme = createTheme({});
 
 ReactDOM.render(
+  <ThemeProvider theme={theme}>
     <BrowserRouter>
-         <App/>
-    </BrowserRouter>,
-    
-document.getElementById('root'))
-
-
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+  document.getElementById("root")
+);
