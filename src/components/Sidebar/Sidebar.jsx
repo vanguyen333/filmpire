1c4426a0495318fef7a0b82c01e864fc780c8002
vanguyen333 <vanguyen333@yahoo.com>
2024-01-15 16:07:40 -0500

    added red and blue logo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -14,12 +14,23 @@ import { useTheme } from "@mui/styles";
 
 import useStyles from "./styles";
 
+const redLogo =
+  "https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png";
+
+const blueLogo =
+  "https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png";
 const Sibar = ({ setMobileOpen }) => {
   const theme = useTheme();
   const classes = useStyles();
   return (
     <>
-      <Link to="/" className={classes.imageLink}></Link>
+      <Link to="/" className={classes.imageLink}>
+        <img
+          alt="filmpire logo"
+          className={classes.image}
+          src={theme.palette.mode === "light" ? redLogo : blueLogo}
+        />
+      </Link>
     </>
   );
 };
