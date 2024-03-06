fc0432356ee5158cb3ac0ed374be58adffbdf859
vanguyen333 <vanguyen333@yahoo.com>
2024-03-06 11:15:40 -0500

    deleted node modules

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import { tmdbApi } from "../services/TMDB";
-
-export default configureStore({
-  reducer: {
-    [tmdbApi.reducerPath]: tmdbApi.reducer,
-  },
-});
