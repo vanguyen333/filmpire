e7864b59202e0d62bb5b04753a764d464047534a
vanguyen333 <vanguyen333@yahoo.com>
2024-03-06 11:09:53 -0500

    deleted components

--- src/components/Actors/Actors.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Actors = () => {
-  return <div>Actors</div>;
-};
-
-export default Actors;
--- src/components/MovieInformation/MovieInformation.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MovieInformation = () => {
-  return <div>MovieInformation</div>;
-};
-
-export default MovieInformation;
