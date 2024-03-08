4b9f7dd0047b90b61c49a8538e4fa2b8dca94a6c
vanguyen333 <vanguyen333@yahoo.com>
2024-03-07 23:10:15 -0500

    the movie image showed up

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -9,23 +9,19 @@ const Movie = ({movie, i }) => {
 
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-        <Grow in key={i} timeout={(i+1)*250}>  
-        <Link className={classes.links} to={`/movie/${movie.id}`}>
-         {/* <img
-            alt={movie.title}
-            className={classes.image}
-            src={movie.poster_path ?`https://image.tmdb.org/t/p/w500/${movie.poster_path}` : 'https://www.fillmurray.com/200/300'}
-          /> */}
-             <img
-            alt={movie.title}
-            className={classes.image}
-            src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : 'https://www.fillmurray.com/200/300'}
-          />
+       <Grow in key={i} timeout={(i + 1) * 250}>
+      <Link className={classes.links} to={`/movie/${movie.id}`}>
+        <img
+          alt={movie.title}
+          className={classes.image}
+          src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : 'https://www.fillmurray.com/200/300'}
+        />
        
-        </Link>
+       
         <Typography className={classes.title} variant='h5' >
         {movie.title}
         </Typography>
+        </Link>
       </Grow>
     </Grid>
   )
