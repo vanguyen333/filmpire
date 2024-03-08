c24d81d3315a1a1f0ae3d837767019455be79df9
vanguyen333 <vanguyen333@yahoo.com>
2024-03-07 19:35:04 -0500

    redux, add middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
 
+// import { tmdbApi } from "../services/TMDB";
+
+// export default configureStore({
+//     reducer: {
+//         [tmdbApi.reducerPath]: tmdbApi.reducer,
+//     }
+// })
+
+
+import { configureStore } from "@reduxjs/toolkit";
 import { tmdbApi } from "../services/TMDB";
 
 export default configureStore({
     reducer: {
         [tmdbApi.reducerPath]: tmdbApi.reducer,
-    }
-})
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(tmdbApi.middleware),
+});
